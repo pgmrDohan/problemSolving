877da04c09ef2be1ebd5029d7526036ff53e83a3
Dohan <dohan.k764@gmail.com>
2024-06-01 14:18:51 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
+++ C++17/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -1,12 +1,9 @@
-#include <cmath>
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
     int n = 0, e = 0, o = 0;
     cin >> n;
-    // 2 3 4 5 6 7 8 9 10
     for (int i = 2; i <= n; i++) {
         int foo = i;
         while (foo % 2 == 0) {
